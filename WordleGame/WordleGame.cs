6c22365c7cb472336ec8815a3ef21d4f849d0e73
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-03-19 19:57:37 +0100

    Added a GameState enum and added prop to WordleGame

--- WordleGame/WordleGame.cs
+++ WordleGame/WordleGame.cs
@@ -5,5 +5,13 @@
 		public string Wordle { get; }
 		public int Guesses { get; }
 		public int GuessesLeft { get; private set; }
+		public GameState GameState { get; private set; }
+	}
+	public enum GameState
+	{
+		NotStarted,
+		Ongoing,
+		Completed,
+		Failed
 	}
 }
