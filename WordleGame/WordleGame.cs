b3af5766f87cf960a582b0815b3c56571090e381
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-03-19 20:01:16 +0100

    Created a Start method

--- WordleGame/WordleGame.cs
+++ WordleGame/WordleGame.cs
@@ -10,8 +10,16 @@
 		{
 			Wordle = wordle;
 			Guesses = guesses;
-			GuessesLeft = guesses;
+			GuessesLeft = 0;
 			GameState = GameState.NotStarted;
+		}
+		public void Start()
+		{
+			if (GameState == GameState.NotStarted)
+			{
+				GameState = GameState.Ongoing;
+				GuessesLeft = Guesses;
+			}
 		}
 	}
 	public enum GameState
