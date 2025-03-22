af085a12057e69e7dd847e69cce672ff44ea4d0f
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-03-22 19:01:30 +0100

    Created a general utilities class and a method for loading resource streams

---
+++ WordleCore/Utils/WordleCoreUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace WordleCore.Utils
+{
+	internal static class WordleCoreUtils
+	{
+		internal static string LoadEmbeddedResource(string resourceName)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+
+			using var stream = assembly.GetManifestResourceStream(resourceName);
+			if (null == stream) throw new FileNotFoundException($"Resource {resourceName} not found.");
+			using var reader = new StreamReader(stream);
+
+			return reader.ReadToEnd();
+		}
+	}
+}
