e69c71c6d3e6d955378ccbdbd280f6e426e7b2a1
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-03-29 18:14:49 +0100

    Started on a basic structure for Console.
    Will create classes and methods later.

--- Wordle_Console/Program.cs
+++ Wordle_Console/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Wordle_Console
+﻿using WordleCore;
+
+namespace Wordle_Console
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press a key to pick an option:");
+            Console.WriteLine("1: Start a new game of Wordle");
+            Console.WriteLine("2: Start a game of Wordle with custom options");
+            Console.WriteLine("3: Exit");
+
+            char k = Console.ReadKey().KeyChar;
+
+            if (k == 1)
+            {
+                WordleGame wordleGame = new();
+                wordleGame.Start();
+                Console.WriteLine("Enter your guess");
+                string guess = Console.ReadLine() ?? "";
+                var wordleResponse = wordleGame.GuessWordle(guess);
+                Console.WriteLine(wordleResponse);
+
+            }
+            else if (k == 2) { }
+            else if (k == 3) { Environment.Exit(0); }
+            else { }
         }
     }
 }
