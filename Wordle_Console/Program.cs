412c6419cb7f9567f3cc9fd89aee2df305be9c2a
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-03-31 13:50:08 +0200

    Created a method for getting wordle options, only handles default for now

--- Wordle_Console/Program.cs
+++ Wordle_Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using WordleCore;
+﻿using Wordle_Console.Models;
+using WordleCore;
 using WordleCore.Enums;
 
 namespace Wordle_Console
@@ -18,56 +19,78 @@ namespace Wordle_Console
             //>> Print alphabet at bottom of console. use Get and SetCursorPosition to jump down and back
             //>> Use backgroundcolor instead of foregroundcolor?
 
+            var wordleOptions = GetWordleOptions();
+            Console.Clear();
+
+            //>> Pass wordleOptions here to a method and return wordleGame
+            WordleGame wordleGame = new();
+
+            wordleGame.Start();
+            Console.WriteLine($"Wordle game started, you have {wordleGame.GuessesLeft} guesses to guess {wordleGame.Wordle}\n");
+
+            Console.WriteLine("Enter your guess");
+
+            while (wordleGame.GuessesLeft > 0)
+            {
+                //>> Create a method using ReadKey to take inputs and block enter while not proper Wordle guess
+                string guess = (Console.ReadLine() ?? "").ToUpper();
+                var wordleResponse = wordleGame.GuessWordle(guess);
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.ForegroundColor = CorrectnessColors[wordleResponse.Correctness[i]];
+                    Console.Write(wordleResponse.Chars[i]);
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+
+                if (wordleGame.GameState == GameState.Completed)
+                {
+                    Console.WriteLine("\nYou guessed the right word!\n");
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey(true);
+                    break;
+                }
+
+                if (wordleGame.GameState == GameState.Failed)
+                {
+                    Console.WriteLine("\nYou did not guess the right word!\n");
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey(true);
+                    break;
+                }
+            }
+        }
+
+        internal static WordleOptions GetWordleOptions()
+        {
             Console.WriteLine("Press a key to pick an option:");
             Console.WriteLine("1: Start a new game of Wordle");
             Console.WriteLine("2: Start a game of Wordle with custom options");
             Console.WriteLine("3: Exit");
 
-            char k = Console.ReadKey(true).KeyChar;
+            WordleOptions? wordleOptions = null;
 
-            if (k == '1')
+            while (wordleOptions == null)
             {
-                Console.Clear();
-                WordleGame wordleGame = new();
-                wordleGame.Start();
-                Console.WriteLine($"Wordle game started, you have {wordleGame.GuessesLeft} guesses to guess {wordleGame.Wordle}\n");
+                char k = Console.ReadKey(true).KeyChar;
 
-                Console.WriteLine("Enter your guess");
-
-                while (wordleGame.GuessesLeft > 0)
+                if (k == '1')
                 {
-                    //>> Create a method using ReadKey to take inputs and block enter while not proper Wordle guess
-                    string guess = (Console.ReadLine() ?? "").ToUpper();
-                    var wordleResponse = wordleGame.GuessWordle(guess);
-                    Console.SetCursorPosition(0, Console.CursorTop - 1);
-                    for (int i = 0; i < 5; i++)
-                    {
-                        Console.ForegroundColor = CorrectnessColors[wordleResponse.Correctness[i]];
-                        Console.Write(wordleResponse.Chars[i]);
-                    }
-                    Console.WriteLine();
-                    Console.ResetColor();
-
-                    if (wordleGame.GameState == GameState.Completed)
-                    {
-                        Console.WriteLine("You guessed the right word!\n");
-                        Console.WriteLine("Press any key to continue");
-                        Console.ReadKey(true);
-                        break;
-                    }
-
-                    if (wordleGame.GameState == GameState.Failed)
-                    {
-                        Console.WriteLine("You did not guess the right word!\n");
-                        Console.WriteLine("Press any key to continue");
-                        Console.ReadKey(true);
-                        break;
-                    }
+                    wordleOptions = new WordleOptions();
+                }
+                else if (k == '2')
+                {
+                    //>> Take inputs
+                }
+                else if (k == '3') { Environment.Exit(0); }
+                else
+                {
+                    //>> Print invalid input
                 }
             }
-            else if (k == '2') { }
-            else if (k == '3') { Environment.Exit(0); }
-            else { }
+
+            return wordleOptions;
         }
 
         internal static Dictionary<Correctness, ConsoleColor> CorrectnessColors = new()
