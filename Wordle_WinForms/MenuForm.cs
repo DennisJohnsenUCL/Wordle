2de1839693bf4c324f33fd3a91568c7e5028cb3f
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-04-12 18:06:08 +0200

    Renamed Form1 to MenuForm

--- Wordle_WinForms/MenuForm.cs
+++ Wordle_WinForms/MenuForm.cs
@@ -1,8 +1,8 @@
 namespace Wordle_WinForms
 {
-    public partial class Form1 : Form
+    public partial class MenuForm : Form
     {
-        public Form1()
+        public MenuForm()
         {
             InitializeComponent();
         }
