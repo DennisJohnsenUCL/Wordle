46b91a6b872bdf0f715f43cfccb1ae3b37f3bae4
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-04-12 23:10:43 +0200

    Created a Custom Control for a bordered Label

---
+++ Wordle_WinForms/CustomControls/LetterLabel.cs
@@ -0,0 +1,18 @@
+﻿namespace Wordle_WinForms.CustomControls
+{
+    internal class LetterLabel : Label
+    {
+        public LetterLabel()
+        {
+            BackColor = Color.LightGray;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            using Pen pen = new(Color.Black, 1);
+            e.Graphics.DrawRectangle(pen, 0, 0, ClientSize.Width - 1, ClientSize.Height - 1);
+        }
+    }
+}
