a232cd931699fc2feef58c11d3707e4dab171953
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-04-13 11:14:34 +0200

    LetterLabel no longer autosizes

--- Wordle_WinForms/CustomControls/LetterLabel.cs
+++ Wordle_WinForms/CustomControls/LetterLabel.cs
@@ -8,6 +8,7 @@
             BackColor = Color.White;
             Size = new Size(40, 40);
             Font = new Font(Font.FontFamily, 20f, FontStyle.Bold);
+            AutoSize = false;
         }
 
         protected override void OnPaint(PaintEventArgs e)
