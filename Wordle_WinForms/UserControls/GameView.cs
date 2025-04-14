5a9c272a0520f3cc7fa473d28466884695e8a1ff
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-04-14 09:46:35 +0200

    GameView now handles letters keys and backspace being pressed

--- Wordle_WinForms/UserControls/GameView.cs
+++ Wordle_WinForms/UserControls/GameView.cs
@@ -1,17 +1,15 @@
-﻿using System.Text;
-using Wordle_WinForms.CustomControls;
+﻿using Wordle_WinForms.CustomControls;
 using WordleCore;
 using WordleCore.Enums;
 using WordleCore.Models;
+using WordleCore.Utils;
 
 namespace Wordle_WinForms.UserControls
 {
     public partial class GameView : UserControl
     {
-        private bool _listening = false;
         private WordleRow? _activeRow = null;
         private WordleGame? _game = null;
-        private string? _newGuess = null;
 
         public GameView()
         {
@@ -27,36 +25,66 @@ namespace Wordle_WinForms.UserControls
             _game.Start();
             var row = new WordleRow();
             WordleRowsFlowPanel.Controls.Add(row);
-            _listening = true;
             _activeRow = row;
-            //var guess = GetWordleGuessInput(row);
-            //var response = game.GuessWordle(guess);
-            //PrintWordleGuessCorrectness(response, row);
         }
 
-        private void PrintWordleGuessCorrectness(WordleResponse response, WordleRow row)
+        private void PrintWordleGuessCorrectness(WordleResponse response)
         {
-            for (int i = 0; i < response.Chars.Length; i++)
+            if (_activeRow != null)
             {
-                row.Controls[i].Text = response.Chars[i].ToString();
-                row.Controls[i].BackColor = _colors[response.Correctness[i]];
+                for (int i = 0; i < response.Chars.Length; i++)
+                {
+                    _activeRow.Controls[i].Text = response.Chars[i].ToString();
+                    _activeRow.Controls[i].BackColor = _colors[response.Correctness[i]];
+                }
             }
-        }
-
-        private string GetWordleGuessInput(WordleRow row)
-        {
-            var sb = new StringBuilder();
-            foreach (LetterLabel control in row.Controls)
-            {
-                sb.Append(control.Text);
-            }
-
-            return sb.ToString();
         }
 
         private void GameView_KeyDown(object sender, KeyEventArgs e)
         {
-            MessageBox.Show(e.KeyData.ToString());
+            var k = e.KeyData;
+            var c = e.KeyCode;
+
+            if (c >= Keys.A && c <= Keys.Z) HandleKeyPress((char)k);
+            if (k == Keys.Enter) HandleEnterPress();
+            else if (k == Keys.Back) HandleBackPress();
+        }
+
+        private void HandleKeyPress(char c)
+        {
+            if (_activeRow != null)
+            {
+                foreach (LetterLabel control in _activeRow.Controls)
+                {
+                    if (control.Text == "") { control.Text = c.ToString(); break; }
+                }
+            }
+        }
+
+        private void HandleEnterPress()
+        {
+            if (_activeRow != null && _game != null)
+            {
+                string guess = GetRow();
+                if (guess.Length == 5 && WordleGameUtils.IsAllowedWord(guess))
+                {
+                    var response = _game.GuessWordle(guess);
+                    PrintWordleGuessCorrectness(response);
+                }
+            }
+
+            string GetRow() { return "guess"; }
+        }
+
+        private void HandleBackPress()
+        {
+            if (_activeRow != null)
+            {
+                for (int i = _activeRow.Controls.Count - 1; i >= 0; i--)
+                {
+                    if (_activeRow.Controls[i].Text != "") { _activeRow.Controls[i].Text = ""; break; }
+                }
+            }
         }
 
         protected override bool ProcessDialogKey(Keys keyData)
