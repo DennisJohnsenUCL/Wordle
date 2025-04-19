c973c2f38b1b00776af9be1c4a3989da5dfbda2c
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-04-19 10:08:06 +0200

    Set up UI elements on OptionsView

--- Wordle_WinForms/UserControls/OptionsView.Designer.cs
+++ Wordle_WinForms/UserControls/OptionsView.Designer.cs
@@ -28,19 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            BackButton = new Button();
+            WordleLabel = new Label();
+            StartGameButton = new Button();
+            WordleTextBox = new TextBox();
+            GuessesUpDown = new NumericUpDown();
+            GuessesLabel = new Label();
+            InvalidWordleLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)GuessesUpDown).BeginInit();
             SuspendLayout();
+            // 
+            // BackButton
+            // 
+            BackButton.Location = new Point(3, 3);
+            BackButton.Name = "BackButton";
+            BackButton.Size = new Size(71, 43);
+            BackButton.TabIndex = 0;
+            BackButton.Text = "Back";
+            BackButton.UseVisualStyleBackColor = true;
+            // 
+            // WordleLabel
+            // 
+            WordleLabel.AutoSize = true;
+            WordleLabel.Location = new Point(246, 118);
+            WordleLabel.Name = "WordleLabel";
+            WordleLabel.Size = new Size(89, 30);
+            WordleLabel.TabIndex = 1;
+            WordleLabel.Text = "Wordle:";
+            // 
+            // StartGameButton
+            // 
+            StartGameButton.AutoSize = true;
+            StartGameButton.Location = new Point(308, 297);
+            StartGameButton.Name = "StartGameButton";
+            StartGameButton.Size = new Size(128, 40);
+            StartGameButton.TabIndex = 3;
+            StartGameButton.Text = "Start game";
+            StartGameButton.UseVisualStyleBackColor = true;
+            // 
+            // WordleTextBox
+            // 
+            WordleTextBox.Location = new Point(392, 115);
+            WordleTextBox.MaxLength = 5;
+            WordleTextBox.Name = "WordleTextBox";
+            WordleTextBox.Size = new Size(120, 36);
+            WordleTextBox.TabIndex = 4;
+            // 
+            // GuessesUpDown
+            // 
+            GuessesUpDown.Location = new Point(392, 199);
+            GuessesUpDown.Maximum = new decimal(new int[] { 999, 0, 0, 0 });
+            GuessesUpDown.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            GuessesUpDown.Name = "GuessesUpDown";
+            GuessesUpDown.Size = new Size(120, 36);
+            GuessesUpDown.TabIndex = 5;
+            GuessesUpDown.Value = new decimal(new int[] { 6, 0, 0, 0 });
+            // 
+            // GuessesLabel
+            // 
+            GuessesLabel.AutoSize = true;
+            GuessesLabel.Location = new Point(246, 201);
+            GuessesLabel.Name = "GuessesLabel";
+            GuessesLabel.Size = new Size(96, 30);
+            GuessesLabel.TabIndex = 6;
+            GuessesLabel.Text = "Guesses:";
+            // 
+            // InvalidWordleLabel
+            // 
+            InvalidWordleLabel.AutoSize = true;
+            InvalidWordleLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            InvalidWordleLabel.ForeColor = Color.Red;
+            InvalidWordleLabel.Location = new Point(392, 154);
+            InvalidWordleLabel.Name = "InvalidWordleLabel";
+            InvalidWordleLabel.Size = new Size(0, 21);
+            InvalidWordleLabel.TabIndex = 7;
             // 
             // OptionsView
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(InvalidWordleLabel);
+            Controls.Add(GuessesLabel);
+            Controls.Add(GuessesUpDown);
+            Controls.Add(WordleTextBox);
+            Controls.Add(StartGameButton);
+            Controls.Add(WordleLabel);
+            Controls.Add(BackButton);
             Font = new Font("Segoe UI", 16F);
             Margin = new Padding(5, 6, 5, 6);
             Name = "OptionsView";
             Size = new Size(744, 461);
+            ((System.ComponentModel.ISupportInitialize)GuessesUpDown).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BackButton;
+        private Label WordleLabel;
+        private Button StartGameButton;
+        private TextBox WordleTextBox;
+        private NumericUpDown GuessesUpDown;
+        private Label GuessesLabel;
+        private Label InvalidWordleLabel;
     }
 }
