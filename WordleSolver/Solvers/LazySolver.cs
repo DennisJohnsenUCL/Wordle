f9b2755cb2df5bdfe17369be4c06a67c6dd946cb
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-07 21:37:33 +0200

    Moved .editorconfig to solution directory. Added a line to suppress "Use primary constructor".

--- WordleSolver/Solvers/LazySolver.cs
+++ WordleSolver/Solvers/LazySolver.cs
@@ -2,33 +2,33 @@
 
 namespace WordleSolver.Solvers
 {
-    internal class LazySolver : ISolver
-    {
-        protected virtual string[] Words { get; }
-        protected int Index { get; set; } = 0;
-        public virtual string Identifier { get; }
+	internal class LazySolver : ISolver
+	{
+		protected virtual string[] Words { get; }
+		protected int Index { get; set; } = 0;
+		public virtual string Identifier { get; }
 
-        public LazySolver(string[] words, string identifier)
-        {
-            Words = words;
-            Identifier = identifier;
-        }
+		public LazySolver(string[] words, string identifier)
+		{
+			Words = words;
+			Identifier = identifier;
+		}
 
-        public virtual string GetFirstGuess()
-        {
-            return GetNextGuess();
-        }
+		public virtual string GetFirstGuess()
+		{
+			return GetNextGuess();
+		}
 
-        public virtual string GetNextGuess()
-        {
-            var guess = Words[Index];
-            Index++;
-            return guess;
-        }
+		public virtual string GetNextGuess()
+		{
+			var guess = Words[Index];
+			Index++;
+			return guess;
+		}
 
-        public virtual void Reset()
-        {
-            Index = 0;
-        }
-    }
+		public virtual void Reset()
+		{
+			Index = 0;
+		}
+	}
 }
