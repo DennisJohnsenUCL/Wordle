0fb561d11720ee29a5195c88747a946305d64fd4
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-10 09:23:03 +0200

    Added worst case tracking to SolverResult.

--- WordleSolver/Controllers/SolverController.cs
+++ WordleSolver/Controllers/SolverController.cs
@@ -6,66 +6,67 @@ using WordleSolver.Models;
 
 namespace WordleSolver.Controllers
 {
-    internal class SolverController
-    {
-        private readonly ISolver _solver;
-        private readonly IEnumerable<WordleGame> _games;
-        private readonly List<int> _guessesList = [];
+	internal class SolverController
+	{
+		private readonly ISolver _solver;
+		private readonly IEnumerable<WordleGame> _games;
+		private readonly List<int> _guessesList = [];
 
-        public SolverController(ISolver solver, IEnumerable<WordleGame> games)
-        {
-            _solver = solver;
-            _games = games;
-        }
+		public SolverController(ISolver solver, IEnumerable<WordleGame> games)
+		{
+			_solver = solver;
+			_games = games;
+		}
 
-        public SolverResult Run()
-        {
-            var timer = new Stopwatch();
+		public SolverResult Run()
+		{
+			var timer = new Stopwatch();
 
-            timer.Start();
-            foreach (var game in _games)
-            {
-                int guesses = ExecuteGame(game);
-                _guessesList.Add(guesses);
-                _solver.Reset();
-            }
-            timer.Stop();
+			timer.Start();
+			foreach (var game in _games)
+			{
+				int guesses = ExecuteGame(game);
+				_guessesList.Add(guesses);
+				_solver.Reset();
+			}
+			timer.Stop();
 
-            double averageGuesses = (double)_guessesList.Sum() / _guessesList.Count;
-            int failedGames = _guessesList.Count(guesses => guesses > 6);
+			double averageGuesses = (double)_guessesList.Sum() / _guessesList.Count;
+			int failedGames = _guessesList.Count(guesses => guesses > 6);
 
-            var result = new SolverResult(_solver.Identifier,
-                averageGuesses,
-                failedGames,
-                _games.Count(),
-                timer.ElapsedMilliseconds);
+			var result = new SolverResult(_solver.Identifier,
+				averageGuesses,
+				failedGames,
+				_guessesList.Max(),
+				_games.Count(),
+				timer.ElapsedMilliseconds);
 
-            return result;
-        }
+			return result;
+		}
 
-        private int ExecuteGame(WordleGame game)
-        {
-            game.Start();
+		private int ExecuteGame(WordleGame game)
+		{
+			game.Start();
 
-            var guess = _solver.GetFirstGuess();
-            var response = game.GuessWordle(guess);
+			var guess = _solver.GetFirstGuess();
+			var response = game.GuessWordle(guess);
 
-            while (!IsGameOver(game))
-            {
-                if (_solver is IReactiveSolver solver) solver.AddResponse(response);
+			while (!IsGameOver(game))
+			{
+				if (_solver is IReactiveSolver solver) solver.AddResponse(response);
 
-                guess = _solver.GetNextGuess();
-                response = game.GuessWordle(guess);
-            }
+				guess = _solver.GetNextGuess();
+				response = game.GuessWordle(guess);
+			}
 
-            int guesses = game.Guesses - game.GuessesLeft;
+			int guesses = game.Guesses - game.GuessesLeft;
 
-            return guesses;
-        }
+			return guesses;
+		}
 
-        private static bool IsGameOver(WordleGame game)
-        {
-            return (game.GameState == GameState.Completed || game.GameState == GameState.Failed);
-        }
-    }
+		private static bool IsGameOver(WordleGame game)
+		{
+			return (game.GameState == GameState.Completed || game.GameState == GameState.Failed);
+		}
+	}
 }
--- WordleSolver/Models/SolverResult.cs
+++ WordleSolver/Models/SolverResult.cs
@@ -1,27 +1,29 @@
 ﻿namespace WordleSolver.Models
 {
-    internal record SolverResult
-    {
-        public string SolverIdentifier { get; }
-        public double AverageGuesses { get; }
-        public int FailedGames { get; }
-        public int GamesPlayed { get; }
-        public long ElapsedMilliseconds { get; }
+	internal record SolverResult
+	{
+		public string SolverIdentifier { get; }
+		public double AverageGuesses { get; }
+		public int FailedGames { get; }
+		public int WorstCase { get; }
+		public int GamesPlayed { get; }
+		public long ElapsedMilliseconds { get; }
 
-        public SolverResult(string solverIdentifier, double averageGuesses, int failedGames, int gamesPlayed, long elapsedMilliseconds)
-        {
-            SolverIdentifier = solverIdentifier;
-            AverageGuesses = averageGuesses;
-            FailedGames = failedGames;
-            GamesPlayed = gamesPlayed;
-            ElapsedMilliseconds = elapsedMilliseconds;
-        }
+		public SolverResult(string solverIdentifier, double averageGuesses, int failedGames, int worstCase, int gamesPlayed, long elapsedMilliseconds)
+		{
+			SolverIdentifier = solverIdentifier;
+			AverageGuesses = averageGuesses;
+			FailedGames = failedGames;
+			WorstCase = worstCase;
+			GamesPlayed = gamesPlayed;
+			ElapsedMilliseconds = elapsedMilliseconds;
+		}
 
-        public override string ToString()
-        {
-            return $"Solver: {SolverIdentifier}\n" +
-                $"Average guesses per Wordle = {AverageGuesses:0.00}, time taken (in ms): {ElapsedMilliseconds}\n" +
-                $"Games played: {GamesPlayed}, failed games: {FailedGames}";
-        }
-    }
+		public override string ToString()
+		{
+			return $"Solver: {SolverIdentifier}\n" +
+				$"Average guesses per Wordle = {AverageGuesses:0.00}, time taken (in ms): {ElapsedMilliseconds}\n" +
+				$"Games played: {GamesPlayed}, failed games: {FailedGames}, Worst case: {WorstCase}";
+		}
+	}
 }
