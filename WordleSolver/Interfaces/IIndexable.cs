1a85be9cee8d47b56f421d1001e9a0b1b5262bb7
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-10 10:45:38 +0200

    Created a Word struct which allows indexing and follows IEnumerable and IEquatable.

---
+++ WordleSolver/Interfaces/IIndexable.cs
@@ -0,0 +1,8 @@
+﻿namespace WordleSolver.Interfaces
+{
+	internal interface IIndexable<T>
+	{
+		T this[int index] { get; }
+		int Length { get; }
+	}
+}
---
+++ WordleSolver/Models/Word.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using WordleSolver.Interfaces;
+
+namespace WordleSolver.Models
+{
+	internal readonly struct Word : IEquatable<Word>, IIndexable<char>, IEnumerable<char>
+	{
+		private readonly char c0, c1, c2, c3, c4;
+
+		public Word(ReadOnlySpan<char> span)
+		{
+			if (span.Length != 5) throw new ArgumentException("Word must be 5 characters long.", nameof(span));
+
+			c0 = span[0];
+			c1 = span[1];
+			c2 = span[2];
+			c3 = span[3];
+			c4 = span[4];
+		}
+
+		public char this[int index] => index switch
+		{
+			0 => c0,
+			1 => c1,
+			2 => c2,
+			3 => c3,
+			4 => c4,
+			_ => throw new IndexOutOfRangeException()
+		};
+
+		public int Length => 5;
+
+		public bool Equals(Word other)
+		{
+			return c0 == other.c0 && c1 == other.c1 && c2 == other.c2 && c3 == other.c3 && c4 == other.c4;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return obj is Word word && Equals(word);
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(c0, c1, c2, c3, c4);
+		}
+
+		public override string ToString() => $"{c0}{c1}{c2}{c3}{c4}";
+
+		public static bool operator ==(Word left, Word right) => left.Equals(right);
+
+		public static bool operator !=(Word left, Word right) => !left.Equals(right);
+
+		public Enumerator GetEnumerator() => new(this);
+
+		IEnumerator<char> IEnumerable<char>.GetEnumerator() => GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		public struct Enumerator : IEnumerator<char>
+		{
+			private readonly Word _word;
+			private int _index;
+
+			public Enumerator(Word word)
+			{
+				_word = word;
+				_index = -1;
+			}
+
+			public readonly char Current => _index switch
+			{
+				0 => _word.c0,
+				1 => _word.c1,
+				2 => _word.c2,
+				3 => _word.c3,
+				4 => _word.c4,
+				_ => throw new InvalidOperationException()
+			};
+
+			readonly object IEnumerator.Current => Current;
+
+			public bool MoveNext() => ++_index < 5;
+			public void Reset() => _index = -1;
+			public readonly void Dispose() { }
+		}
+	}
+}
