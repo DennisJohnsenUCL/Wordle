ebe6d309d927fd0fc1b9561a597ee22021d69c37
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-11 09:47:07 +0200

    Updated ConstraintsManager to enforce OverCount letters being present elsewhere in the word.

--- WordleSolver/Core/ConstraintManager.cs
+++ WordleSolver/Core/ConstraintManager.cs
@@ -5,56 +5,51 @@ using WordleSolver.Models;
 
 namespace WordleSolver.Core
 {
-    internal class ConstraintManager : IConstraintManager
-    {
-        private HashSet<Constraint> _constraints = [];
-        private Dictionary<char, int> _counts = [];
+	internal class ConstraintManager : IConstraintManager
+	{
+		private HashSet<Constraint> _constraints = [];
+		private Dictionary<char, int> _counts = [];
 
-        public void AddConstraints(IReadOnlyList<LetterResult> results)
-        {
-            for (int i = 0; i < results.Count; i++)
-            {
-                var (letter, correctness) = results[i];
+		public void AddConstraints(IReadOnlyList<LetterResult> results)
+		{
+			for (int i = 0; i < results.Count; i++)
+			{
+				var (letter, correctness) = results[i];
 
-                if (correctness == Correctness.OverCount)
-                {
-                    var count = results.Count(r => r.Letter == letter &&
-                    (r.Correctness == Correctness.Present || r.Correctness == Correctness.Correct));
+				if (correctness == Correctness.OverCount)
+				{
+					var count = results.Count(r => r.Letter == letter &&
+					(r.Correctness == Correctness.Present || r.Correctness == Correctness.Correct));
+					_counts.TryAdd(letter, count);
+				}
+				var constraint = new Constraint(letter, correctness, i);
+				_constraints.Add(constraint);
+			}
+		}
 
-                    _counts.TryAdd(letter, count);
-                }
-                else
-                {
-                    var constraint = new Constraint(letter, correctness, i);
+		public bool FitsConstraints(string word)
+		{
+			foreach (var constraint in _constraints)
+			{
+				var (letter, correctness, position) = constraint;
 
-                    _constraints.Add(constraint);
-                }
-            }
-        }
+				if (correctness == Correctness.Absent && word.Contains(letter)) return false;
+				if (correctness == Correctness.Correct && word[position] != letter) return false;
+				if (correctness == Correctness.Present && !word.Remove(position, 1).Contains(letter)) return false;
+				if (correctness == Correctness.OverCount && !word.Remove(position, 1).Contains(letter)) return false;
+			}
 
-        public bool FitsConstraints(string word)
-        {
-            foreach (var constraint in _constraints)
-            {
-                var (letter, correctness, position) = constraint;
+			foreach (var count in _counts)
+			{
+				if (word.Count(c => c == count.Key) != count.Value) return false;
+			}
+			return true;
+		}
 
-                if (correctness == Correctness.Absent && word.Contains(letter)) return false;
-                if (correctness == Correctness.Correct && word[position] != letter) return false;
-                if (correctness == Correctness.Present && !word.Remove(position, 1).Contains(letter)) return false;
-            }
-
-            foreach (var count in _counts)
-            {
-                if (word.Count(c => c == count.Key) > count.Value) return false;
-            }
-
-            return true;
-        }
-
-        public void Clear()
-        {
-            _constraints = [];
-            _counts = [];
-        }
-    }
+		public void Clear()
+		{
+			_constraints = [];
+			_counts = [];
+		}
+	}
 }
